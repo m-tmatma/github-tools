fcd8eba9ac23f17df9507a62548ff459151f9cd2
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-08-11 13:58:43 +0900

    add LICENCE

--- enum-releases.py
+++ enum-releases.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 #
+# MIT License
+#
+# Copyright (c) [2017] [Masaru Tsuchiyama]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
 #   This script enumerates releases for github.com
 #
 #   usage: ./enum-releases.py owner repos
@@ -9,6 +32,7 @@
 #       
 #       This script accesses the following site, extract the release versions, and show them.
 #       https://api.github.com/repos/m-tmatma/CopyWithLineNumbers/releases
+#
 
 """
 enum releases at github.com and returns them as a list
